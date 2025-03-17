3c3e0a5274a6b728ec264fb750200c38bc2fc6e1
cloudofdarkhole6 <bernd.schimek@gmail.com>
2025-03-17 13:27:43 +0100

    Layout work

--- scripts/items_import.lua
+++ scripts/items_import.lua
@@ -1,6 +1,6 @@
 
 Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/hint_items.json")
-Tracker:AddItems("items/location_items.json")
-Tracker:AddItems("items/labels.json")
+Tracker:AddItems("items/psynergy.json")
+Tracker:AddItems("items/events.json")
+Tracker:AddItems("items/settings.json")
                 
