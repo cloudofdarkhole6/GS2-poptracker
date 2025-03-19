6fd92c2daea16ce29d12e84f73da427751380c47
cloudofdarkhole6 <bernd.schimek@gmail.com>
2025-03-19 10:52:07 +0100

    Begin cleanup of locations

--- scripts/locations_import.lua
+++ scripts/locations_import.lua
@@ -1,2 +1,4 @@
 Tracker:AddLocations("locations/Weyard.json")
-Tracker:AddLocations("locations/Overworld.json")
+Tracker:AddLocations("locations/airs_rock_exterior.json")
+Tracker:AddLocations("locations/airs_rock_interior.json")
+Tracker:AddLocations("locations/dehkan_plateau.json")
